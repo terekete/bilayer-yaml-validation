8c1cd1e28eb3e346e89a57d2c197fb87b066d184
gatesma <4775500+terekete@users.noreply.github.com>
2022-03-31 00:30:21 -0400

    chore: add scalar functions

---
+++ schemas/scalar_function.py
@@ -0,0 +1,72 @@
+{
+    'kind': {
+        'required': True,
+        'type': 'string',
+        'nullable': False,
+        'regex': '^scalar_function$'
+    },
+    'version': {
+        'required': True,
+        'type': 'string',
+        'nullable': False,
+        'regex': 'v(\d{1})',
+    },
+    'metadata': {
+        'required': True,
+        'type': 'dict',
+        'schema': {
+            'dep': {
+                'required': True,
+                'type': 'string',
+                'nullable': False
+            }
+        }
+    },
+    'description': {
+        'required': True,
+        'type': 'string',
+        'nullable': False
+    },
+    'resource_name': {
+        'required': True,
+        'type': 'string',
+        'nullable': False,
+        'regex': '[a-z0-9_]+'
+    },
+    'dataset_id': {
+        'required': True,
+        'type': 'string',
+        'nullable': False
+    },
+    'language': {
+        'required': True,
+        'type': 'string',
+        'nullable': False
+    },
+    'return_type': {
+        'required': True,
+        'type': 'string',
+        'nullable': False
+    },
+    'input_parameters': {
+        'required': True,
+        'type': 'list',
+        'nullable': False,
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'name': {
+                    'type': 'string'
+                },
+                'data_type': {
+                    'type': 'string'
+                }
+            }
+        }
+    },
+    'definition_body': {
+        'required': True,
+        'type': 'string',
+        'nullable': False
+    }
+}
