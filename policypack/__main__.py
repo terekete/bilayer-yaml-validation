af986e3ab7d87afaf955ef0cfb800c61e193b40a
dongqiaoyang <65913434+dongqiaoyang@users.noreply.github.com>
2022-10-24 09:25:48 -0400

    updated policy

--- policypack/__main__.py
+++ policypack/__main__.py
@@ -6,6 +6,9 @@ from pulumi_policy import (
     ResourceValidationPolicy,
 )
 
+import json
+
+
 
 def storage_bucket_no_public_read_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
     if args.resource_type == "gcp:storage/bucketACL:BucketACL" and "predefinedAcl" in args.props:
@@ -31,12 +34,54 @@ location = ResourceValidationPolicy(
     validate=location_validator,
 )
 
+json_data = json.load(open("allowed_resources.json"))
+
+
+
+def resources_types_allow_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if args.resource_type not in json_data["allow_list"]:
+        report_violation("resource type not allowed")
+
+resources_types_allow = ResourceValidationPolicy(
+    name="resources-types-allow",
+    description="Only allow build of certain types of resources",
+    validate=resources_types_allow_validator,
+)
+
+
+def bq_datatset_role_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if args.resource_type == "gcp:bigquery/datasetIamMember:DatasetIamMember":
+        role = args.props["role"]
+        if role != "roles/bigquery.dataViewer":
+            report_violation("Dataset binding can only set role: roles/bigquery.dataViewer")
+
+bq_datatset_role = ResourceValidationPolicy(
+    name="bq_datatset_role",
+    description="Dataset binding can only set role: roles/bigquery.dataViewer",
+    validate=bq_datatset_role_validator,
+)
+
+def bq_table_role_validator(args: ResourceValidationArgs, report_violation: ReportViolation):
+    if args.resource_type in ['google-native:bigquery/v2:TableIamMember', 'gcp:bigquery/iamMember:IamMember']:
+        role = args.props["role"]
+        if role not in ["roles/bigquery.dataViewer", "roles/bigquery.dataOwner"]:
+            report_violation("Table binding can only set role: roles/bigquery.dataViewer, roles/bigquery.dataOwner")
+
+bq_table_role = ResourceValidationPolicy(
+    name="bq_table_role",
+    description="Table binding can only set role: roles/bigquery.dataViewer, roles/bigquery.dataOwner",
+    validate=bq_table_role_validator,
+)
+
+
 PolicyPack(
     name="gcp-python",
     enforcement_level=EnforcementLevel.MANDATORY,
     policies=[
         storage_bucket_no_public_read,
-        location
+        location,
+        resources_types_allow,
+        bq_datatset_role,
+        bq_table_role
     ],
 )
-
